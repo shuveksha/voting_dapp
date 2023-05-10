8861aab466bc87256e9198b710c83f83cfa8682d
Admin <subashbaidya9@gmail.com>
2023-05-10 19:49:31 +0545

    Result Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import AddCandidate from './pages/Admin/AddCandidate';
 import AdminHome from './pages/Admin/AdminHome';
+import Results from './pages/Results';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/AddCandidate' element={<AddCandidate/>}/>
           <Route path='/AdminHome' element={<AdminHome/>}/>
+          <Route path='/Results' element={<Results/>}/>
         </Routes>
       </BrowserRouter>
     </div>
