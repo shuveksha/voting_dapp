9fdd62760a97472a97bd5bd593d6e822ef9c07f0
Suman Parajuli <sumansupercell@gmail.com>
2023-05-12 12:44:14 +0545

    added walletconnected component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import AddCandidate from './pages/Admin/AddCandidate';
 // user route 
 import Home from './pages/User/Home';
+import WalletConnected from './pages/User/WalletConnected';
 // user route 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      {/* wallet connected component  */}
+          <WalletConnected/>
         <Routes>
           {/* user route  */}
-          <Route path='/' element={<Home/>} />
+          {/* <Route path='/' element={<Home/>} /> */}
           {/* user route  */}
 
           <Route path='/AddCandidate' element={<AddCandidate/>}/>
